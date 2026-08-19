4beaa8430012a7ba034960f4ab30d76e7aa07575
Shlok Choudhury <eldenlord20111@gmail.com>
2026-08-19 18:17:25 +0530

    Stable release

---
+++ src/main.cpp
@@ -0,0 +1,179 @@
+#include <raylib.h>
+
+Color Green = Color{38, 185, 154, 255};
+Color Dark_Green = Color{20, 160, 133, 255};
+Color Light_Green = Color{129, 204, 184, 255};
+Color Yellow = Color{243, 213, 91, 255};
+
+int player_score = 0;
+int cpu_score = 0;
+
+class Ball{
+    public:
+        float x, y;
+        int speed_x, speed_y;
+        int radius;
+
+        void Draw(){
+            DrawCircle(x, y, radius, Yellow);
+        }
+        void Update(){
+            x += speed_x;
+            y += speed_y;
+
+            if (y + radius >= GetScreenHeight() || y - radius <= 0) {
+                speed_y *= -1;
+            }
+
+            // CPU wins
+            if (x + radius >= GetScreenWidth()) {
+                cpu_score++;
+                ResetBall();
+            }
+
+            if (x - radius <= 0) {
+                player_score++;
+                ResetBall();
+            }
+        }
+
+        void ResetBall() {
+            x = GetScreenWidth() / 2;
+            y = GetScreenHeight() / 2;
+
+            int speed_choices[2] = {-1, 1};
+            speed_x *= speed_choices[GetRandomValue(0, 1)];
+            speed_y *= speed_choices[GetRandomValue(0, 1)];
+        }
+};
+
+class Paddle{
+    protected:
+        void LimitMovement() {
+            if (y <= 0) {
+                y = 0;
+            }
+            if (y + height >= GetScreenHeight()) {
+                y = GetScreenHeight() - height;
+            }
+        }
+
+    public:
+        float x, y;
+        float width, height;
+        int speed;
+
+        void Draw(){
+            DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
+        }
+
+        void Update() {
+            if (IsKeyDown(KEY_UP)) {
+                y = y - speed;
+            }
+            if (IsKeyDown(KEY_DOWN)) {
+                y = y + speed;
+            }
+
+            LimitMovement();
+        }
+};
+
+class CpuPaddle : public Paddle{
+    public:
+        float tolerance = 25;
+
+        void Update(int ball_y, int ball_speed_x){
+            if (ball_speed_x < 0) {
+                if (y + height / 2 > ball_y + tolerance) {
+                    y = y - speed;
+                }
+                if (y + height / 2 <= ball_y - tolerance) {
+                    y = y + speed;
+                }
+            } else {
+                float center = GetScreenHeight() / 2.0f - height / 2.0f;
+                if (y + height / 2 < center - tolerance) {
+                    y += speed / 2;
+                }
+                if (y + height / 2 > center + tolerance) {
+                    y -= speed / 2;
+                }
+            }
+
+            LimitMovement();
+        }
+};
+
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
+
+int main(){
+
+    int screen_width = 1280;
+    int screen_height = 800;
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT); 
+
+    InitWindow(screen_width, screen_height, "Pong");
+    SetTargetFPS(60);
+
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height / 2 - player.height / 2;
+    player.speed = 6;
+
+    cpu.height = 120;
+    cpu.width = 25;
+    cpu.x = 10;
+    cpu.y = screen_height / 2 - cpu.height / 2;
+    cpu.speed = 6;
+
+    while (WindowShouldClose() == false){
+
+        // Updating
+
+        ball.Update();
+        player.Update();
+        cpu.Update(ball.y, ball.speed_x);
+
+        // Checking for collision
+
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height})){
+            ball.speed_x *= -1;
+        }
+
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height})){
+            ball.speed_x *= -1;
+        }
+
+        // Drawing
+        BeginDrawing();
+        ClearBackground(Dark_Green);
+
+        DrawRectangle(screen_width / 2, 0, screen_width / 2, screen_height, Green);
+        DrawCircle(screen_width / 2, screen_height / 2, 150, Light_Green);
+
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        ball.Draw();
+        player.Draw();
+        cpu.Draw();
+
+        DrawText(TextFormat("%i", cpu_score), screen_width / 4 - 20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", player_score), 3 * screen_width / 4 - 20, 20, 80, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
